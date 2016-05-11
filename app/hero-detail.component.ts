452562f51a55d08a0b059832cdc32aec71a959b4
Anton Platonov <platosha@gmail.com>
2016-05-11 15:13:46 +0300

    Add hero detail editor

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouteParams } from '@angular/router-deprecated';
+import { PolymerElement } from 'vaadin-ng2-polymer/polymer-element';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -8,9 +9,35 @@ import { HeroService } from './hero.service';
   selector: 'my-hero-detail',
   template: `
     <div *ngIf="hero">
-      <h2>{{hero.name}} details</h2>
+      <paper-input label="Name" [(value)]="hero.name"></paper-input>
+      <vaadin-date-picker label="Birthday" [(value)]="hero.birthday"></vaadin-date-picker>
     </div>
-  `
+  `,
+  directives: [
+    PolymerElement('paper-input'),
+    PolymerElement('vaadin-date-picker')
+  ],
+  styles: [`
+    :host {
+      display: block;
+      padding: 16px;
+    }
+
+    paper-input[label="Name"] {
+      --paper-input-container-label: {
+        @apply(--paper-font-display1);
+        transition: transform 0.25s, width 0.25s, font 0.25s;
+      };
+
+      --paper-input-container-label-floating: {
+        @apply(--paper-font-subhead);
+      };
+
+      --paper-input-container-input: {
+        @apply(--paper-font-display1);
+      };
+    }
+  `]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
